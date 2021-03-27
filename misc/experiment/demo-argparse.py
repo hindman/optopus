5173ba6595aa50b536f5104531f6ba8ff402eba5
Monty Hindman <mhindman@apple.com>
2021-03-26 20:01:11 -0700

    Planning, notes, etc

--- misc/experiment/demo-argparse.py
+++ misc/experiment/demo-argparse.py
@@ -4,7 +4,7 @@ import sys
 import argparse
 
 ARG_PARSE_DATA = [
-    [['url_groups'],     dict(nargs = '*', choices = ['A', 'B'])],
+    [['url_groups'],     dict(nargs = 2)],
     [['-s', '--sleep'],  dict(type = float, default = 1.0, metavar = 'SEC')],
     [['-y'],             dict(type = int, metavar = 'N')],
     [['-d', '--dryrun'], dict(action = 'store_true')],
@@ -17,7 +17,7 @@ ap = argparse.ArgumentParser()
 for xs, kws in ARG_PARSE_DATA:
     ap.add_argument(*xs, **kws)
 
-opts = ap.parse_args()
+opts = ap.parse_known_args()
 
 print(opts)
 
