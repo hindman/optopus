de00046a85e261f2c93110ec6501ff4b4c1d89f7
Monty Hindman <mhindman@apple.com>
2021-04-11 20:14:21 -0700

    Notes

--- misc/experiment/demo-argparse.py
+++ misc/experiment/demo-argparse.py
@@ -3,18 +3,22 @@
 import sys
 import argparse
 
-ARG_PARSE_DATA = [
-    [['url_groups'],     dict(nargs = 2)],
-    [['-s', '--sleep'],  dict(type = float, default = 1.0, metavar = 'SEC')],
-    [['-y'],             dict(type = int, metavar = 'N')],
-    [['-d', '--dryrun'], dict(action = 'store_true')],
-    [['-x'],             dict(action = 'append', nargs = 2)],
-    [['-z'],             dict(nargs = '+')],
-]
+ACTIONS = 'create read update delete'.split()
+
+ARG_PARSE_DATA = (
+    ['groups',         dict(nargs = 2)],
+    ['-s', '--sleep',  dict(type = float, default = 1.0, metavar = 'SEC')],
+    ['-y',             dict(type = int, metavar = 'N')],
+    ['-d', '--dryrun', dict(action = 'store_true')],
+    ['-x',             dict(action = 'append', nargs = 2)],
+    ['-z',             dict(nargs = '+')],
+    ['--op',           dict(choices = ACTIONS, help = 'The operation to perform')],
+)
 
 ap = argparse.ArgumentParser()
 
-for xs, kws in ARG_PARSE_DATA:
+for xs in ARG_PARSE_DATA:
+    kws = xs.pop()
     ap.add_argument(*xs, **kws)
 
 opts = ap.parse_args()
