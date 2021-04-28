0255b95fac957cb999d17eb66ad5387014236a82
Monty Hindman <mhindman@apple.com>
2021-04-27 23:53:32 -0700

    Notes

--- misc/experiment/demo-argparse.py
+++ misc/experiment/demo-argparse.py
@@ -6,13 +6,14 @@ ACTIONS = 'create read update delete'.split()
 
 ARG_PARSE_DATA = (
     ['groups',         dict(nargs = 2, metavar = 'GRP')],
+    ['items',          dict(nargs = 2, metavar = 'IT')],
     ['-s', '--sleep',  dict(type = float, default = 1.0, metavar = 'SEC')],
     ['-y',             dict(type = int, metavar = 'N')],
     ['-d', '--dryrun', dict(action = 'store_true')],
     ['-x',             dict(action = 'append', nargs = 2)],
     ['-z',             dict(nargs = '+')],
     ['--op',           dict(choices = ACTIONS, help = 'The operation to perform')],
-    ['--1h',           dict()],  # works but you must use getattr(opts, '1h').
+    # ['--1h',           dict()],  # works but you must use getattr(opts, '1h').
 )
 
 ap = argparse.ArgumentParser()
