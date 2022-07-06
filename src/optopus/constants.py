a01a8219da663c9bb8755050a86a1c4128541c8a
Monty Hindman <mhindman@apple.com>
2022-07-06 11:11:27 -0700

    Added TODO for needed regex fix

--- src/optopus/constants.py
+++ src/optopus/constants.py
@@ -21,6 +21,9 @@ RGXS = constants(
     'Regexes',
     dict(
         short_option = re.compile(r'^ - ([a-z]) $', re.X + re.I),
+        # TODO: fix.
+        # (1) Don't use \w. Use [a-z0-9]
+        # (2) Catastrophic backtracking due to quantifiers in repeated groups.
         long_option = re.compile(r'^ -- ( [a-z]\w* ([_-]\w+)* ) $', re.X + re.I),
         negative_num = re.compile(r'^ - \d+ $', re.X),
     ),
