16454ea8b3d72b7259b9b8d31f3a50216a28af1b
Monty Hindman <mhindman@apple.com>
2025-04-09 00:00:42 -0700

    Mockups

--- misc/examples/click/click-naval-fate.py
+++ misc/examples/click/click-naval-fate.py
@@ -1,13 +1,13 @@
 
 '''
-
-TODO: collect the help text
-    - very tedious
-
+See the optopus implementation for notes.
 '''
 
-import click
+####
+# Click.
+####
 
+import click
 
 @click.group()
 @click.version_option()
@@ -19,7 +19,6 @@ def cli():
     is not all that interesting.
     """
 
-
 @cli.group()
 def ship():
     """Manages ships."""
@@ -30,7 +29,6 @@ def ship_new(name):
     """Creates a new ship."""
     click.echo(f"Created ship {name}")
 
-
 @ship.command("move")
 @click.argument("ship")
 @click.argument("x", type=float)
@@ -40,7 +38,6 @@ def ship_move(ship, x, y, speed):
     """Moves SHIP to the new location X,Y."""
     click.echo(f"Moving ship {ship} to {x},{y} with speed {speed}")
 
-
 @ship.command("shoot")
 @click.argument("ship")
 @click.argument("x", type=float)
@@ -49,11 +46,9 @@ def ship_shoot(ship, x, y):
     """Makes SHIP fire to X,Y."""
     click.echo(f"Ship {ship} fires to {x},{y}")
 
-
 @cli.group("mine")
 def mine():
     """Manages mines."""
-
 
 @mine.command("set")
 @click.argument("x", type=float)
@@ -70,7 +65,6 @@ def mine_set(x, y, ty):
     """Sets a mine at a specific coordinate."""
     click.echo(f"Set {ty} mine at {x},{y}")
 
-
 @mine.command("remove")
 @click.argument("x", type=float)
 @click.argument("y", type=float)
@@ -80,4 +74,107 @@ def mine_remove(x, y):
 
 if __name__ == '__main__':
     cli()
+
+####
+# Click help texts.
+####
+
+'''
+
+naval-fate --help
+
+    Usage: naval-fate [OPTIONS] COMMAND [ARGS]...
+
+      Naval Fate.
+
+      This is the docopt example adopted to Click but with some actual commands
+      implemented and not just the empty parsing which really is not all that
+      interesting.
+
+    Options:
+      --version  Show the version and exit.
+      --help     Show this message and exit.
+
+    Commands:
+      mine  Manages mines.
+      ship  Manages ships.
+
+naval-fate mine --help
+
+    Usage: naval-fate mine [OPTIONS] COMMAND [ARGS]...
+
+      Manages mines.
+
+    Options:
+      --help  Show this message and exit.
+
+    Commands:
+      remove  Removes a mine at a specific coordinate.
+      set     Sets a mine at a specific coordinate.
+
+naval-fate mine remove --help
+
+    Usage: naval-fate mine remove [OPTIONS] X Y
+
+      Removes a mine at a specific coordinate.
+
+    Options:
+      --help  Show this message and exit.
+
+naval-fate mine set --help
+
+    Usage: naval-fate mine set [OPTIONS] X Y
+
+      Sets a mine at a specific coordinate.
+
+    Options:
+      --moored    Moored (anchored) mine. Default.
+      --drifting  Drifting mine.
+      --help      Show this message and exit.
+
+naval-fate ship --help
+
+    Usage: naval-fate ship [OPTIONS] COMMAND [ARGS]...
+
+      Manages ships.
+
+    Options:
+      --help  Show this message and exit.
+
+    Commands:
+      move   Moves SHIP to the new location X,Y.
+      new    Creates a new ship.
+      shoot  Makes SHIP fire to X,Y.
+
+naval-fate ship move --help
+
+    Usage: naval-fate ship move [OPTIONS] SHIP X Y
+
+      Moves SHIP to the new location X,Y.
+
+    Options:
+      --speed KN  Speed in knots.
+      --help      Show this message and exit.
+
+naval-fate ship move new --help
+
+    Usage: naval-fate ship move [OPTIONS] SHIP X Y
+
+      Moves SHIP to the new location X,Y.
+
+    Options:
+      --speed KN  Speed in knots.
+      --help      Show this message and exit.
+
+naval-fate ship move new shoot --help
+
+    Usage: naval-fate ship move [OPTIONS] SHIP X Y
+
+      Moves SHIP to the new location X,Y.
+
+    Options:
+      --speed KN  Speed in knots.
+      --help      Show this message and exit.
+
+'''
 
--- misc/examples/optopus-mock-ups/optopus-naval-fate.py
+++ misc/examples/optopus-mock-ups/optopus-naval-fate.py
@@ -1,33 +1,114 @@
 #! /usr/bin/env python
 
-# The docopt naval-fate example
+'''
 
-ORIGINAL = '''
-Naval Fate.
+naval-fate: Optopus vs docopt vs click.
 
-Usage:
-  naval-fate ship new <name>...
-  naval-fate ship <name> move <x> <y> [--speed=<kn>]
-  naval-fate ship shoot <x> <y>
-  naval-fate mine (set|remove) <x> <y> [--moored|--drifting]
-  naval-fate -h | --help
-  naval-fate --version
+Configuration of the parser:
 
-Options:
-  -h --help     Show this screen.
-  --version     Show version.
-  --speed=<kn>  Speed in knots [default: 10].
-  --moored      Moored (anchored) mine.
-  --drifting    Drifting mine.
+    - Docopt and optopus are both compact and intuitive:
+        - Docopt is slightly better on that front.
+        - But at the cost having almost no features beyond the core.
+        - The small increment of added complexity in the optopus configuration
+          exists precisely to rememdy a core weakness of docopt: it's poor
+          structuring of returned data.
+
+    - Click configuration:
+        - Much more verbose and repetitive.
+        - A sea of API calls.
+        - The code provides no easy way to see the program's usage at a glance.
+        - The model is not readily amenable to putting the parser configuration
+          in a data structure. Instead, the configuration must be spread across
+          separate decorators.
+
+Help text:
+
+    - Readable and intutive:
+        - Yes: optopus and docopt.
+        - No: click.
+        - Click's help is convoluted:
+            - User never gets a complete view.
+            - Must invoke --help in several ways to see everything.
+
+    - User control over structure, content, details:
+        - High: optopus.
+        - Moderately high, but with a hard cap: docopt.
+        - Very little: click.
+
+    - Dynamic with terminal:
+        - Yes: optopus and click.
+        - No: docopt: it's static.
+
+    - Only optopus gives both:
+        - High degree of user control [optopus, docopt].
+        - Delegation of help-text convention adherence [optopus, click].
+
+Parsed data:
+
+    - Docopt:
+        - Strange, hyper-flatted data.
+        - User would have to convert it to something usable.
+
+    - Click:
+        - Reasonable.
+        - But hyper-localized to the values relevant to the current command.
+
+    - Optopus:
+        - Usable and holistic data out of the gate.
+
+Size: 
+    108% vs docopt
+    44% vs click
+
+Help size:
+    127% vs docopt
+    26% vs click
 
 '''
 
-SPEC = '''
+####
+# Optopus.
+####
 
+SPEC = '''
 <weapon=ship>        <action=new>        <name>...
 <weapon=ship> <name> <action=move>       <x> <y> [--speed <kn>]
 <weapon=ship>        <action=shoot>      <x> <y>
 <weapon=mine>        <action=set|remove> <x> <y> [--moored | --drifting]
 
+<name>       : Ship name
+<x>          : X coordinate
+<y>          : Y coordinate
+--speed <kn> : Speed in knots [default: 10]
+--moored     : Moored (anchored) mine
+--drifting   : Drifting mine
+'''
+
+from optopus import Parser
+
+p = Parser(SPEC, version = '2.0')
+p.config_help_text(options_summary = False)
+opts = p.parse()
+
+####
+# Optopus help text.
+####
+
+'''
+Usage:
+  naval-fate ship new <name>...
+  naval-fate ship <name> move <x> <y> [--speed <kn>]
+  naval-fate ship shoot <x> <y>
+  naval-fate mine <set|remove> <x> <y> [--moored | --drifting]
+
+Arguments:
+  <name>                 Ship name
+  <x>                    X coordinate
+  <y>                    Y coordinate
+  --speed <kn>           Speed in knots [default: 10]
+  --moored               Moored (anchored) mine
+  --drifting             Drifting mine
+  --version              Print program version and exit
+  --help, -h             Print help text and exit
 '''
 
